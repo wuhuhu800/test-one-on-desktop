5d4fd91b4f5e310381cff0ded50db2c879874df2
阿虎 <wuhuhu800@126.com>
2017-08-24 22:17:04 +0800

    edit2 modify

--- 开智作业/ch2/practice3-2.py
+++ 开智作业/ch2/practice3-2.py
@@ -3,22 +3,26 @@ import sys
 import requests
 import ast
 
-url = "https://api.seniverse.com/v3/weather/now.json"
+url = "https://api.seniverse.com/v3/weather/now.json" #知心天气API
 UserInput = input('请输入所要查询的天气>>> ')
 
 SeachLog = {}
 def SearchWeather(UserInput):
     try:
         while True:
-            paramsdict ={'key': 'lom0ftfaihx65jh0','location':UserInput ,'language': 'zh-Hans','unit': 'c'} 
+            paramsdict ={'key': 'lom0ftfaihx65jh0','location':UserInput ,'language': 'zh-Hans','unit': 'c'}
             req = requests.get(url,params=paramsdict,timeout =1)
-            m = ast.literal_eval(req.text)
-            print('%s的天气为%s' %(UserInput,m['results'][0]['now']['text'])) 
-            print('风向为%s,温度为%s摄氏度'%(m['results'][0]['now']['wind_direction'],m['results'][0]['now']['temperature']))
-            print('更新时间为:%s' %m['results'][0]['last_update'])
-            SeachLog[UserInput] = m['results'][0]['now']['text']
+            m= req.json()#知心天气接口返回值为str，需要转为dict
+            weather =m['results'][0]['now']['text']
+            wind =m['results'][0]['now']['wind_direction']#风向
+            temperature =m['results'][0]['now']['temperature']#温度
+            lasttime =m['results'][0]['last_update']#更新时间
+            print('北京的天气为%s' %weather)
+            print('风向为%s,温度为%s摄氏度'%(wind,temperature))
+            print('更新时间为:%s' %lasttime)
+            SeachLog[UserInput] = weather
             UserInput = input('请输入城市名称>> ')
-    except KeyError:
+    except KeyError: # keyerror抓取
 
         if UserInput == 'help':
             print('''
@@ -40,5 +44,5 @@ def SearchWeather(UserInput):
             print("该城市目前没有被收录")
             UserInput = input('请重新输入城市名称>> ')
             SearchWeather(UserInput)
-            
+
 SearchWeather(UserInput)
--- 开智作业/ch2/practice3-3.py
+++ 开智作业/ch2/practice3-3.py
@@ -10,19 +10,18 @@ url = "https://api.seniverse.com/v3/weather/now.json"
 #SeachLog = {}
 #def SearchWeather(UserInput):
 #    try:
-#while True:
-paramsdict ={'key': 'lom0ftfaihx65jh0','location':'北京' ,'language': 'zh-Hans','unit': 'c'} 
-req = requests.get(url,params=paramsdict,timeout =1)
-#    m = ast.literal_eval(req.text)
-m= req.json()
-print(m)
-weather =m['results'][0]['now']['text']
-wind =m['results'][0]['now']['wind_direction']
-temperature =m['results'][0]['now']['temperature']
-lasttime =m['results'][0]['last_update']
+while True:
+    paramsdict ={'key': 'lom0ftfaihx65jh0','location':'北京' ,'language': 'zh-Hans','unit': 'c'}
+    req = requests.get(url,params=paramsdict,timeout =1)
 
-print('北京的天气为%s' %weather) 
-print('风向为%s,温度为%s摄氏度'%(wind,temperature))
-print('更新时间为:%s' %lasttime)
-#    time.sleep(3)#每隔3秒刷新一次
-            
+    m= req.json()
+    print(m)
+    weather =m['results'][0]['now']['text']
+    wind =m['results'][0]['now']['wind_direction']
+    temperature =m['results'][0]['now']['temperature']
+    lasttime =m['results'][0]['last_update']
+
+    print('北京的天气为%s' %weather)
+    print('风向为%s,温度为%s摄氏度'%(wind,temperature))
+    print('更新时间为:%s' %lasttime)
+    time.sleep(3)#每隔3秒刷新一次
