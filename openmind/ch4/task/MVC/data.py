5427a1c680cf654e3483309290fc35cef3b7d12d
阿虎 <wuhuhu800@126.com>
2017-09-14 18:01:37 +0800

    new3

--- openmind/ch4/task/MVC/data.py
+++ openmind/ch4/task/MVC/data.py
@@ -2,23 +2,40 @@ import practicetest
 import sqlite3
 con = sqlite3.connect('weather.db')
 cur = con.cursor()
-city = ['北京']
-#UserInputCity = input('请输入所要查询的城市>>>')
+UserInputCity = input('请输入所要查询的城市>>>')
+city = [UserInputCity]#传入一个用户输入的城市，为了这句话用：format(', '.join('?' for _ in city)), city
+#
+
 ListForCompare =[]
 weatherday = '多云'
 cur.execute('SELECT * FROM WeatherData WHERE City in ({0})'.format(', '.join('?' for _ in city)), city)
 # 上面这句查询语句作用就是能把一横行的数据都查出来
 row = cur.fetchall()
-if row == ListForCompare:#如果没有查询的内容为空的话
-    print(1)
-
+#    以下if完成两件事情
 #    1、调用接口查数据
 #    2、查到数据保存在数据库里。
-else:
+if row == ListForCompare:#如果数据库里没有查询的城市的情况
+    print(1)
+    #以下是调用practicetest的函数SearchWeather的返回值
+    WeatherDayDaily = practicetest.SearchWeather(UserInputCity)[1]
+    WeatherNightDaily = practicetest.SearchWeather(UserInputCity)[2]
+    TemperatureHighDaily = practicetest.SearchWeather(UserInputCity)[3]
+    TemperatureLowDaily = practicetest.SearchWeather(UserInputCity)[4]
+    WindDirectionDaily = practicetest.SearchWeather(UserInputCity)[5]
+    WindScaleDaily = practicetest.SearchWeather(UserInputCity)[6]
+    #以下是将函数返回值插入数据库
+    cur.execute(
+    "INSERT INTO WeatherData(City, WeatherDay,WeatherNight,TemperatureHigh,TemperatureDailylow,WindDirection,WindScale)\
+     VALUES(?,?,?,?,?,?,?)",\
+     (UserInputCity,WeatherDayDaily,WeatherNightDaily,TemperatureHighDaily,TemperatureLowDaily,WindDirectionDaily,WindScaleDaily))
+    cur.execute('SELECT * FROM WeatherData WHERE City in ({0})'.format(', '.join('?' for _ in city)), city)
+    row1 = cur.fetchall()
+    print(row1)
+
+else:#如果查询的城市在数据库里，直接显示
     print(row)
 
-#    pass 1、返回要查的内容
-#    2、每隔3小时查询一次数据库，并且更新数pass据库
+
 #HistoryMessage={}
 #cur.execute('''CREATE TABLE WeatherData
 
